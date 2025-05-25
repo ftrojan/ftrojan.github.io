d31fe3f7f3252e76f8fd7c53ed85897b55af3d93
filip <f.trojan@centrum.cz>
2025-05-25 11:02:31 +0200

    filter just future events

--- src/pages/EventsPage.vue
+++ src/pages/EventsPage.vue
@@ -1,10 +1,28 @@
 <template>
   <q-page class="flex flex-center">
     <h1>Vystoupení</h1>
-    <p>{{ data }}</p>
+    <q-card v-for="event in futureEvents" :key="event.id">
+      <q-card-section>
+        <p>Kdy: {{ event.date }}</p>
+        <p>Kde: {{ event.location }}</p>
+        <img :src="photoUrl(event.poster_id)" alt="Poster">
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
 <script setup>
+import { computed } from 'vue';
 import data from 'src/assets/events.json';
+
+const photoUrl = (fileId) => {
+  const url = `https://lh3.googleusercontent.com/d/${fileId}`;
+  return url;
+};
+
+const today = new Date();
+
+const futureEvents = computed(() =>
+  data.filter(event => new Date(event.date) > today)
+);
 </script>
